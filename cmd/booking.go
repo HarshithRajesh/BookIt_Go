2e0ecfd0ba5371828a09a0a9c763aad116e6aeea
harshith <harshithr2004@gmail.com>
2026-01-29 16:10:40 +0530

    working: in memory booking created

--- cmd/booking.go
+++ cmd/booking.go
@@ -1,1 +1,34 @@
+// Package Cmd is used for cli booking tickets
 package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/HarshithRajesh/BookIt_Go/storage"
+
+	"github.com/spf13/cobra"
+)
+
+var bookingCmd = &cobra.Command{
+	Use:   "booking",
+	Short: "Booking Tickets",
+	Long:  "Book the event tickets",
+	Run: func(cmd *cobra.Command, args []string) {
+		eventID := args[0]
+		customerName := args[1]
+		customerPhone := args[2]
+		BookedTickets := args[3]
+
+		err := storage.CheckEventTickets(eventID, BookedTickets)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(eventID, customerName, customerPhone, BookedTickets)
+		storage.CreateBooking(eventID, BookedTickets, customerName, customerPhone)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(bookingCmd)
+}
--- storage/memory.go
+++ storage/memory.go
@@ -2,6 +2,11 @@
 package storage
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+
 	"github.com/HarshithRajesh/BookIt_Go/models"
 )
 
@@ -13,10 +18,58 @@ var events = []models.Event{
 	{ID: 5, EventName: "Golang International conference", Date: "20 May 2026", TotalTickets: 100, RemainingTickets: 100},
 }
 
+var booking = []models.Booking{}
+
 func GetEvents() []string {
 	var names []string
 	for _, event := range events {
 		names = append(names, event.EventName)
 	}
 	return names
+}
+
+func CheckEventTickets(eventID1 string, BookedTickets1 string) error {
+	eventID, err := strconv.Atoi(eventID1)
+	if err != nil {
+		log.Fatal("Couldnt convert Event id to int")
+	}
+	BookedTickets, err := strconv.Atoi(BookedTickets1)
+	if err != nil {
+		log.Fatal("Couldnt convert BookedTickets to int")
+	}
+	for _, event := range events {
+		if event.ID == eventID {
+			if event.RemainingTickets > BookedTickets {
+				return nil
+			} else if event.RemainingTickets > 0 {
+				fmt.Println("RemainingTickets is ", event.RemainingTickets)
+				return errors.New("remainingTickets is less then booking ticketsg")
+
+			} else {
+				return errors.New("remainingTickets is 0")
+			}
+		}
+	}
+
+	return errors.New("event not found")
+}
+
+func CreateBooking(eventID1 string, BookedTickets1 string, customerName string, customerPhone string) {
+	eventID, err := strconv.Atoi(eventID1)
+	if err != nil {
+		log.Fatal("Couldnt convert Event id to int")
+	}
+	BookedTickets, err := strconv.Atoi(BookedTickets1)
+	if err != nil {
+		log.Fatal("Couldnt convert BookedTickets to int")
+	}
+	newBooking := models.Booking{
+		ID:            len(booking) + 1,
+		CustomerName:  customerName,
+		CustomerPhone: customerPhone,
+		EventID:       eventID,
+		BookedTickets: BookedTickets,
+	}
+	booking = append(booking, newBooking)
+	fmt.Println("Booking Successful")
 }
